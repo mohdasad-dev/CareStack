6b0c2f275ea41a0abcfe4d248dde97a00ff8cc41
MD ASAD <mdasadgodargama@gmail.com>
2026-05-09 14:49:52 +0530

    feat(admin): build admin dashboard and complete admin panel - developed admin dashboard page - displayed key system data and latest appointments - integrated dashboard with backend APIs - implemented appointment management features - completed core admin panel functionalities

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -12,6 +12,8 @@ const AdminContextProvider = (props) => {
     const [doctors, setDoctors] = useState([]);
     const [appointments, setAppointments] = useState([]);
 
+    const [dashData, setDashData] = useState(false);
+
     const backendURL = import.meta.env.VITE_BACKEND_URL
 
     const getAllDoctors = async (req, res) => {
@@ -63,6 +65,34 @@ const AdminContextProvider = (props) => {
     }
 
 
+    const cancelAppointment = async (appointmentId) => {
+        try {
+            const {data} = await axios.post(backendURL + '/api/admin/cancel-appointment' , {appointmentId} , {headers:{aToken}});
+            if(data.success){
+                toast.success(data.message);
+                getAllAppointments();
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+
+    const getDashData = async () => {
+        try {
+            const {data} = await axios.get(backendURL + '/api/admin/dashboard', {headers:{aToken}});
+            if(data.success){
+                setDashData(data.dashData);
+                console.log(data.dashData);
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
 
     const value = {
         aToken, setAToken,
@@ -71,7 +101,10 @@ const AdminContextProvider = (props) => {
         changeAvailability,
         appointments,
         setAppointments,
-        getAllAppointments
+        getAllAppointments,
+        cancelAppointment,
+        getDashData,
+        dashData
     }
     return(
         <AdminContext.Provider value={value}>
--- admin/src/pages/Admin/AllApointments.jsx
+++ admin/src/pages/Admin/AllApointments.jsx
@@ -1,9 +1,12 @@
 import React, { useContext, useEffect } from 'react'
 import { AdminContext } from '../../context/AdminContext'
+import { AppContext } from '../../context/AppContext';
+import { assets } from '../../assets/assets';
 
 const AllApointments = () => {
 
-  const {aToken, appointments, getAllAppointments} = useContext(AdminContext);
+  const {aToken, appointments, getAllAppointments, cancelAppointment} = useContext(AdminContext);
+  const {calculateAge, slotDateFormat, currency} = useContext(AppContext)
 
   useEffect(() => {
     if(aToken){
@@ -36,6 +39,23 @@ const AllApointments = () => {
             <div className='flex items-center gap-2'>
               <img className='w-8 rounded-full' src={item.userData.image} alt="" /> <p> {item.userData.name}</p>
             </div>
+
+            <p className='max-sm:hidden'>{calculateAge(item.userData.dob)}</p>
+
+            <p>{slotDateFormat(item.slotDate)}, {item.slotTime} </p>
+
+            <div className='flex items-center gap-2'>
+              <img className='w-8 rounded-full bg-gray-200' src={item.docData.image} alt="" /> <p> {item.docData.name}</p>
+            </div>
+
+            <p> {currency} {item.amount} </p>
+
+            {
+              item.cancelled
+              ? <p className='text-red-400 text-xs font-medium'>Cancelled</p>
+              : <img onClick={() => cancelAppointment(item._id)} className='w-10 cursor-pointer' src={assets.cancel_icon} alt="" />
+            }
+            
           </div>
         ))}
 
